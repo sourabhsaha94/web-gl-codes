35a223018e310f2aaba53f01646cd477dc1d785a
Sourabh Saha <sourabhsaha@Sourabhs-MacBook-Air.local>
2017-08-27 18:35:28 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,53 +1,89 @@
-  class Color {
-    constructor(r,g,b,a){
-      this.r = r;
-      this.g = g;
-      this.b = b;
-      this.a = a;
-    }
+/* classes */
 
-    change(r,g,b,a){
-      this.r = r;
-      this.g = g;
-      this.b = b;
-      this.a = a;
-    }
-  }
+// Color constructor
+class Color {
+    constructor(r,g,b,a) {
+        try {
+            if ((typeof(r) !== "number") || (typeof(g) !== "number") || (typeof(b) !== "number") || (typeof(a) !== "number"))
+                throw "color component not a number";
+            else if ((r<0) || (g<0) || (b<0) || (a<0))
+                throw "color component less than 0";
+            else if ((r>255) || (g>255) || (b>255) || (a>255))
+                throw "color component bigger than 255";
+            else {
+                this.r = r; this.g = g; this.b = b; this.a = a;
+            }
+        } // end try
 
-  function drawPixel(imagedata,x,y,color){
-    try{
-      if((x<0)||(y<0)||(x>=imagedata.width)||(y>=imagedata.height)){
-        throw "drawpixel is out of bounds";
-      }
-      else if(color instanceof Color){
-        var pixelindex = (y*imagedata.width+x)*4;
-        imagedata.data[pixeldata] = color.r;
-        imagedata.data[pixeldata+1]=color.g;
-        imagedata.data[pixeldata+2]=color.b;
-        imagedata.data[pixeldata+3]=color.a;
-      }
-      else{
-        throw "drawpixel is not instance of COlor";
-      }
-    }
-    catch(e){
-      console.log(e);
-    }
-  }
+        catch (e) {
+            console.log(e);
+        }
+    } // end Color constructor
 
-  function main() {
-    var canvas = document.getElementById('viewport');
-    var context = canvas.getContext('2d');
-    var w = context.canvas.width;
-    var h = context.canvas.height;
+        // Color change method
+    change(r,g,b,a) {
+        try {
+            if ((typeof(r) !== "number") || (typeof(g) !== "number") || (typeof(b) !== "number") || (typeof(a) !== "number"))
+                throw "color component not a number";
+            else if ((r<0) || (g<0) || (b<0) || (a<0))
+                throw "color component less than 0";
+            else if ((r>255) || (g>255) || (b>255) || (a>255))
+                throw "color component bigger than 255";
+            else {
+                this.r = r; this.g = g; this.b = b; this.a = a;
+            }
+        } // end throw
+
+        catch (e) {
+            console.log(e);
+        }
+    } // end Color change method
+} // end color class
+
+
+/* utility functions */
+
+// draw a pixel at x,y using color
+function drawPixel(imagedata,x,y,color) {
+    try {
+        if ((typeof(x) !== "number") || (typeof(y) !== "number"))
+            throw "drawpixel location not a number";
+        else if ((x<0) || (y<0) || (x>=imagedata.width) || (y>=imagedata.height))
+            throw "drawpixel location outside of image";
+        else if (color instanceof Color) {
+            var pixelindex = (y*imagedata.width + x) * 4;
+            imagedata.data[pixelindex] = color.r;
+            imagedata.data[pixelindex+1] = color.g;
+            imagedata.data[pixelindex+2] = color.b;
+            imagedata.data[pixelindex+3] = color.a;
+        } else
+            throw "drawpixel color is not a Color";
+    } // end try
+
+    catch(e) {
+        console.log(e);
+    }
+} // end drawPixel
+
+
+/* main -- here is where execution begins after window load */
+
+function main() {
+
+    // Get the canvas, context, and image data
+    var canvas = document.getElementById("viewport");
+    var context = canvas.getContext("2d");
+    var w = context.canvas.width; // as set in html
+    var h = context.canvas.height;  // as set in html
     var imagedata = context.createImageData(w,h);
 
-    var c = new Color(0,255,0,255);
-    for(var x=350;x<800;x++){
-      for(var y=350;y<800;y++){
-        drawPixel(imagedata,x,y,c);
-      }
-    }
+    // Draw a rectangle with pixels
+    var c = new Color(0,255,0,255); // the color at the pixel: black opaque
+    for (var x=350; x<800; x++)
+        for (var y=350; y<675; y++) {
+            drawPixel(imagedata,x,y,c);
+            // console.log("draw at " +x+ " " +y);
+        }
 
-    context.putImageData(imagedata,0,0);
-  }
+    context.putImageData(imagedata, 0, 0); // display the image in the context
+}
